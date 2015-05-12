5060bbfa71270354f6c0276ed754c62ff66924b5
Eric West <eric@makechip.com>
2015-05-12 08:24:22 -0700

    backup

--- src/test/stdf/StdfTest1.java
+++ src/test/stdf/StdfTest1.java
@@ -13,7 +13,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.makechip.stdf2xls4.stdf.*;
-import com.makechip.util.Log;
 
 /**
  * @author eric
@@ -113,6 +112,7 @@ public class StdfTest1
 	    assertEquals(0, r.getSequenceNumber());
     }
     
+	// atrs.add(new AuditTrailRecord(snum++, dnum, 100000000L, "cmdline"));
 	@Test
 	public void test3()
 	{
@@ -124,7 +124,230 @@ public class StdfTest1
         assertEquals("cmdline", atr.getCmdLine());
 	}
 
-   
+	//MasterInformationRecord mir = new MasterInformationRecord(snum++, dnum, 1000L, 2000L, (short) 1,
+	//	'A', 'B', 'C', 100, 'D', "lotID",
+	//	"partType", "nodeName", "testerType", "jobName", "jobRevisionNumber",  "sublotID", "operatorName",
+	//	"execSoftware", "execSoftwareVersion", "stepCode", "temperature", "userText", "auxDataFile",
+	//	"packageType", "familyID", "dateCode", "facilityID", "floorID","fabID", "frequency", "specName",
+	//	"specVersion", "flowID", "setupID", "designRevision", "engLotID", "romCodeID", "testerSerialNumber",
+	//	"supervisorID", 1234L);
+	@Test
+	public void test4()
+	{
+		StdfRecord r = stack.pop();
+		assertTrue(r instanceof MasterInformationRecord);
+		MasterInformationRecord mir = (MasterInformationRecord) r;
+		assertEquals(2, mir.getSequenceNumber());
+		assertEquals(1, mir.getDeviceNumber());
+		assertTrue((new Date(1000000L).toString().equals(mir.getJobDate())));
+		assertTrue((new Date(2000000L).toString().equals(mir.getTestDate())));
+		assertEquals((short) 1, mir.getStationNumber());
+		assertEquals('A', mir.getTestModeCode());
+		assertEquals('B', mir.getLotRetestCode());
+		assertEquals('C', mir.getDataProtectionCode());
+		assertEquals(100, mir.getBurnInTime());
+		assertEquals('D', mir.getCmdModeCode());
+		assertEquals("lotID", mir.getLotID());
+		assertEquals("partType", mir.getPartType());
+		assertEquals("nodeName", mir.getNodeName());
+		assertEquals("testerType", mir.getTesterType());
+		assertEquals("jobName", mir.getJobName());
+		assertEquals("jobRevisionNumber", mir.getJobRevisionNumber());
+		assertEquals("sublotID", mir.getSublotID());
+		assertEquals("operatorName", mir.getOperatorName());
+		assertEquals("execSoftware", mir.getExecSoftware());
+		assertEquals("execSoftwareRevision", mir.getExecSoftwareVersion());
+		assertEquals("stepCode", mir.getStepCode());
+		assertEquals("temperature", mir.getTemperature());
+		assertEquals("userText", mir.getUserText());
+		assertEquals("auxDataFile", mir.getAuxDataFile());
+		assertEquals("packageType", mir.getPackageType());
+		assertEquals("familyID", mir.getFamilyID());
+		assertEquals("dateCode", mir.getDateCode());
+		assertEquals("facilityID", mir.getFacilityID());
+		assertEquals("floorID", mir.getFloorID());
+		assertEquals("fabID", mir.getFabID());
+		assertEquals("frequency", mir.getFrequency());
+		assertEquals("specName", mir.getSpecName());
+		assertEquals("specVersion", mir.getSpecVersion());
+		assertEquals("flowID", mir.getFlowID());
+		assertEquals("setupID", mir.getSetupID());
+		assertEquals("designRevision", mir.getDesignRevision());
+		assertEquals("engLotID", mir.getEngLotID());
+		assertEquals("romCodeID", mir.getRomCodeID());
+		assertEquals("testerSerialNumber", mir.getTesterSerialNumber());
+		assertEquals("supervisorID", mir.getSupervisorID());
+		assertEquals(1234L, mir.getFileTimeStamp());
+	}
+	
+	// RetestDataRecord rdr = new RetestDataRecord(snum++, dnum, new int[] { 1, 2, 3, 4 });
+	@Test
+	public void test5()
+	{
+		StdfRecord r = stack.pop();
+		assertTrue(r instanceof RetestDataRecord);
+		RetestDataRecord rdr = (RetestDataRecord) r;
+		assertEquals(2, rdr.getSequenceNumber());
+		assertEquals(1, rdr.getDeviceNumber());
+		
+	}
+	
+	//sdrs.add(new SiteDescriptionRecord(snum++, dnum, (short) 1, (short) 2, (short) 1, new int[] { 0 },
+	//	"handlerType", "handlerID", "probeCardType", "probeCardID", "loadboardType", "loadboardID",
+	//	"dibBoardType", "dibBoardID", "ifaceCableType", "ifaceCableID", "contactorType", "contactorID",
+	//	"laserType", "laserID", "equipType", "equipID"));
+	@Test
+	public void test6()
+	{
+	
+	}
+	
+	//stdf.add(new BeginProgramSelectionRecord(snum++, dnum, "beginProgramSelectionRecord"));
+	@Test
+	public void test7()
+	{
+	
+	}
+	
+	//stdf.add(new DatalogTextRecord(snum++, dnum, "datalogTextRecord"));
+	@Test
+	public void test8()
+	{
+	
+	}
+	
+	//stdf.add(new FunctionalTestRecord(snum++, dnum, 3, (short) 2, (short) 1, EnumSet.noneOf(TestFlag_t.class),
+	//	EnumSet.noneOf(FTROptFlag_t.class), 1234L, 111L, 222L, 55L, 4, 5, (short) 6, new int[] { 1, 2, 3, 4 },
+	//	new byte[] { (byte) 4, (byte) 3, (byte) 2, (byte) 1 }, new int[] { 3, 4, 5, 6 },
+	//	new byte[] { (byte) 0, (byte) 1, (byte) 2, (byte) 3 }, new byte[] { (byte) 3, (byte) 2, (byte) 1, (byte) 0 },
+	//	"vecName", "timeSetName", "vecOpCode", "label", "alarmName", "progTxt", "rsltTxt", (short) 5, 
+	//	new byte[] { (byte) 6, (byte) 7, (byte) 8 }));
+	@Test
+	public void test9()
+	{
+	
+	}
+	
+	//GenericDataRecord.Data d1 = new GenericDataRecord.Data(new GenericDataRecord.PadData(Data_t.I_4, 0), new Integer(33));
+	//GenericDataRecord.Data d2 = new GenericDataRecord.Data(new GenericDataRecord.PadData(Data_t.R_8, 0), new Double(44.0));
+	//List<GenericDataRecord.Data> lgd = new ArrayList<GenericDataRecord.Data>(); 
+	//lgd.add(d1);
+	//lgd.add(d2);
+	//stdf.add(new GenericDataRecord(snum++, dnum, lgd));
+	@Test
+	public void test10()
+	{
+	
+	}
+	
+	//stdf.add(new HardwareBinRecord(snum++, dnum, (short) 1, (short) 0, 1, 10L, 'P', "binName"));
+	@Test
+	public void test11()
+	{
+	
+	}
+	
+	//stdf.add(new MasterResultsRecord(snum++, dnum, 1000L, 'C', "lotDesc", "execDesc"));
+	@Test
+	public void test12()
+	{
+	
+	}
+	
+	//stdf.add(new MultipleResultParametricRecord(snum++, dnum, 22, 1, 0, EnumSet.noneOf(TestFlag_t.class),
+	//	EnumSet.noneOf(ParamFlag_t.class), 2, 4, new byte[] { 1, 2 }, new double[] { 1.0, 2.0, 3.0, 4.0 },
+	//	"text", "alarmName", EnumSet.noneOf(OptFlag_t.class), (byte) 0, (byte) 1, (byte) 2, 1.0f, 3.0f,
+	//	0.0f, 0.0f, new int[] { 5, 6 }, "units", "unitsIn", "resFmt", "llmFmt", "hlmFmt", 3.0f, 4.0f));
+	@Test
+	public void test13()
+	{
+	
+	}
+	
+	//stdf.add(new ParametricTestRecord(snum++, dnum, 44, 1, 0, EnumSet.noneOf(TestFlag_t.class),
+	//	EnumSet.noneOf(ParamFlag_t.class), 5.5f, "text", "alarmName", EnumSet.noneOf(OptFlag_t.class),
+	//	(byte) 1, (byte) 2, (byte) 3, 1.0f, 10.0f, "units", "resFmt", "llmFmt", "hlmFmt", 1.0f, 2.0f));
+	@Test
+	public void test14()
+	{
+	
+	}
+	
+	//stdf.add(new PartCountRecord(snum++, dnum, (short) 1, (short) 0, 2L, 1L, 0L, 2L, 1L));
+	@Test
+	public void test15()
+	{
+	
+	}
+	
+	//stdf.add(new PartInformationRecord(snum++, dnum, (short) 1, (short) 0));
+	@Test
+	public void test16()
+	{
+	
+	}
+	
+	//stdf.add(new PartResultsRecord(snum++, dnum, (short) 1, (short) 0, (byte) 0, 1, 2, 3, (short) 1, (short) 2,
+	//	10L, "partID", "partDescription", new byte[] { (byte) 0, (byte) 1, (byte) 2 }));
+	@Test
+	public void test17()
+	{
+	
+	}
+	
+	//stdf.add(new PinListRecord(snum++, dnum, new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 2, 2 }, 
+	//	new String[] { "a", "b" }, new String[] { "c", "d" }, new String[] { "e", "f" }, new String[] { "g", "h" }));
+	@Test
+	public void test18()
+	{
+	
+	}
+	
+	//stdf.add(new PinMapRecord(false, snum++, dnum, 2, 3, "channelName", "physicalPinName", "logicalPinName", (short) 1, (short) 0));
+	@Test
+	public void test19()
+	{
+	
+	}
+	
+	//stdf.add(new SoftwareBinRecord(snum++, dnum, (short) 1, (short) 0, 5, 45, "F", "binName"));
+	@Test
+	public void test20()
+	{
+	
+	}
+	
+    //stdf.add(new TestSynopsisRecord(snum++, dnum, (short) 1, (short) 0, 'T', 10L, 11L, 12L, 13L, "testName",   
+    //	"sequencerName", "testLabel", EnumSet.noneOf(TestOptFlag_t.class), 3.0f, 1.0f, 2.2f, 3.3f, 4.4f));	
+	@Test
+	public void test21()
+	{
+	
+	}
+	
+    //stdf.add(new WaferConfigurationRecord(snum++, dnum, 6.0f, 3.3f, 4.4f, (short) 1, 'L', (short) 1, (short) 2, '5', '5'));
+	@Test
+	public void test22()
+	{
+	
+	}
+	
+    //stdf.add(new WaferInformationRecord(snum++, dnum, (short) 1, (short) 0, 1000L, "waferID"));
+	@Test
+	public void test23()
+	{
+	
+	}
+	
+	//stdf.add(new WaferResultsRecord(snum++, dnum, (short) 1, (short) 0, 1000L, 1L, 2L, 0L, 1L, 0L,
+	//	"waferID", "fabWaferID", "waferFrameID", "waferMaskID", "userWaferDesc", "execWaferDesc"));
+	@Test
+	public void test24()
+	{
+	
+	}
+	
+
 		
 		
 		
